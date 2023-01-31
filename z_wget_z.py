67032a9dff5fec1dd0080c633a93545e80a2e367
barncatreborn <96750441+barncatreborn@users.noreply.github.com>
2023-01-31 22:25:15 +0000

    added word cloud stuff

---
+++ z_wget_z.py
@@ -0,0 +1,6 @@
+import wget
+
+wget.download("https://raw.githubusercontent.com/barncatreborn/word_cloud_z/main/word_cloud_z.py")
+wget.download("https://raw.githubusercontent.com/barncatreborn/word_cloud_z/main/barncat_tfdc.csv")
+
+print("ok")
